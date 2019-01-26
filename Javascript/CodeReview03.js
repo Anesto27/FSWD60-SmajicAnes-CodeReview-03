c7fb58a9b4a58fdc4ec8a060a3973ca6fe836258
Anesto27 <anes.2503@gmx.at>
2019-01-26 15:25:07 +0100

    final version 1.1

--- Javascript/CodeReview03.js
+++ Javascript/CodeReview03.js
@@ -1,25 +1,24 @@
 
 /* create a function called calculateInsurance and added 3 values */
-function calculateInsurance(name, horse_power, age) {
+function calculateInsurance(name, age, horse_power,) {
 /* declared 4 variables and used getElementbyid to take the value of the each ids*/
 /* also used parseFloat only to get the number values otherwise-->(NaN)*/
 		var name = document.getElementById("input1").value;
+		var age =  parseFloat(document.getElementById("input2").value);
 		var horse_power = parseFloat(document.getElementById("input3").value);
-		var age =  parseFloat(document.getElementById("input2").value); 
-
-	 	var country = document.getElementById("select").value;
+		var country = document.getElementById("select").value;
 /*used an If statement and two elses for the three countries */
-			if (country == "austria") {
+			if (country == "Austria") {
 				document.getElementById("result").innerHTML = name + ", your insurance costs " + 
-				Math.floor(horse_power * 100 / age + 50) + "€.";
+				Math.floor(horse_power * 100 / age + 50) + "€!";
 
-			} else if (country == "hungary") {
+			} else if (country == "Hungary") {
 				document.getElementById("result").innerHTML = name + ", your insurance costs " + 
-				Math.floor(horse_power * 120 / age + 100) + "€.";
+				Math.floor(horse_power * 120 / age + 100) + "€!";
 
 			} else { /*Greece */
 				document.getElementById("result").innerHTML = name + ", your insurance costs " + 
-				Math.floor(horse_power * 150 / (age + 3) + 150) + "€.";
+				Math.floor(horse_power * 150 / (age + 3) + 150) + "€!";
 			}
 		}
 		/* get the output with the eventlistener of the calculation*/
